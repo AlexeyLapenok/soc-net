ee6326f0bffe63add09f8cd9cbc328f6066c0656
LapenokAlexey <aleks.lapenok@gmail.com>
2021-11-06 14:05:54 +0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,6 @@
+const Header = () => {
+    return (<header className='header'>
+        <img src='https://image.shutterstock.com/image-vector/initial-logo-im-play-geometric-600w-1888809928.jpg' alt='' />
+    </header>);
+}
+export default Header;
---
+++ src/components/Profile.js
@@ -0,0 +1,20 @@
+const Profile = () => {
+    return (<div className='content'>
+        <div>
+            <img src='https://img4.goodfon.ru/original/2048x1365/4/de/zelen-fon-trava.jpg' alt='' width='100%' height='400px' />
+        </div>
+        <div>
+            <div>
+                ava+ desc
+            </div>
+            <div>
+                Mypost
+            </div>
+            <div>new  post</div>
+            <div>post1</div>
+            <div>post2</div>
+
+        </div>
+    </div>);
+}
+export default Profile;
